704a7c2b2319ece5f6e53c8c4655e7657444f4c2
Eito Katagiri <eitoball@gmail.com>
2018-11-06 10:41:47 +0900

    add CSS for fotorama

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -3,6 +3,10 @@ const vue =  require('./loaders/vue')
 const webpack = require('webpack')
 
 environment.loaders.append('vue', vue)
+// fotorama の CSS に含まれる url のパスを解決する。sass-loader の後、css-loader の
+// 前に resolve-url-loader を入れる必要がある。
+const sass = environment.loaders.get('sass')
+sass.use.splice(sass.use.length - 2, 0, { loader: 'resolve-url-loader' })
 
 const provide = new webpack.ProvidePlugin({
   $: 'jquery',
